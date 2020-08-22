cfe879472864637e0a51c9936290197fc6abb9f1
huangkai <h1770360848@outlook.com>
2020-08-22 16:06:03 +0800

    style: 优化注释文本格式

--- leetcode/0001/two_sum.go
+++ leetcode/0001/two_sum.go
@@ -1,4 +1,4 @@
-/* Two Sum
+/* 1.Two Sum
 
 References: https://leetcode.com/problems/two-sum/
 
@@ -12,7 +12,7 @@ package leetcode
 //
 // * 时间复杂度: O(N^2)，其中N为数组中的元素数量
 // * 空间复杂度: O(1)
-
+//
 func twoSum1(nums []int, target int) []int {
 	for i, v := range nums {
 		for j := i + 1; j < len(nums); j++ {
@@ -27,12 +27,13 @@ func twoSum1(nums []int, target int) []int {
 // twoSum2 哈希表
 // 创建一个哈希表，遍历数组中的每一个数 `x`
 // 判断 `target - x` 是否在哈希表中：
-// 如果在那么直接返回；
-// 如果不存在将 `x` 的值作为key, `x`在数组中的位置 `index` 作为value存入哈希表中
+// 	   1. 如果在那么直接返回；
+//     2. 如果不存在将 `x` 的值作为key, `x`在数组中的位置 `index` 作为value存入哈希表中
 // 这样可以将每个找 `target - x` 的时间复杂度降到O(1)级别
 //
 // * 时间复杂度: O(N)，其中N为数组中的元素数量
 // * 空间复杂度: O(N)，其中N为数组中的元素数量，主要为创建哈希表的开销
+//
 func twoSum2(nums []int, target int) []int {
 	hashTable := map[int]int{}
 	for index, num := range nums {
