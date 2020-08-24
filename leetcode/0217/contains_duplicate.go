ee298db21964b29a74f61d920d38bc6339b504c9
huangkai <h1770360848@outlook.com>
2020-08-25 00:06:48 +0800

    feat: 增加0217问题排序解法

--- leetcode/0217/contains_duplicate.go
+++ leetcode/0217/contains_duplicate.go
@@ -8,6 +8,8 @@ References:
 
 package leetcode
 
+import "sort"
+
 // containsDuplicate 哈希表
 //
 // 创建一个哈希表，遍历数组的每一个数
@@ -23,6 +25,24 @@ func containsDuplicate(nums []int) bool {
 			return true
 		}
 		m[num] = 0
+	}
+	return false
+}
+
+// containsDuplicate 排序
+//
+// 在对数字从小到大排序之后，数组的重复元素一定出现在相邻位置中
+// 遍历已排序的数组，判断相邻的两个元素是否相等，如果相等则说明存在重复的元素
+//
+// * 时间复杂度: O(NlogN)，其中N为数组中的元素数量，需要对数组进行排序
+// * 空间复杂度: O(logN)，其中N为数组中的元素数量
+//
+func containsDuplicate1(nums []int) bool {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
+		}
 	}
 	return false
 }
